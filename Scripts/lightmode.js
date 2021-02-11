351cdb8db3fb7f678da1a6e3f119dac72e5cf71f
RaptorXZ <ouatis@gmail.com>
2021-02-11 11:50:19 +0100

    changes made to light mode button

--- Scripts/lightmode.js
+++ Scripts/lightmode.js
@@ -2,8 +2,11 @@ function lightMode() {
     var element = document.body;
     element.classList.toggle("light-mode");
 
-    var x = document.getElementById("lightmodebutton");
-     if (x.innerHTML === "Toggle Light Mode") {
+    var button = document.getElementById("lightmodebutton");
+    button.classList.toggle("dark-button");
+
+    var x = document.getElementById("lightmodecaption");
+    if (x.innerHTML === "Toggle Light Mode") {
      x.innerHTML = "Toggle Dark Mode";
      } else {
      x.innerHTML = "Toggle Light Mode";
