c9a520653992be283a073753bf1542d83615d946
RaptorXZ <ouatis@gmail.com>
2021-02-11 17:11:48 +0100

    added portfolio slideshow and github link

---
+++ Scripts/gototop.js
@@ -0,0 +1,6 @@
+mybutton = document.getElementById("gototop");
+
+function goToTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- Scripts/lightmode.js
+++ Scripts/lightmode.js
@@ -2,8 +2,11 @@ function lightMode() {
     var element = document.body;
     element.classList.toggle("light-mode");
 
-    var button = document.getElementById("lightmodebutton");
-    button.classList.toggle("dark-button");
+    var button1 = document.getElementById("lightmodebutton");
+    button1.classList.toggle("dark-button");
+
+    var button2 = document.getElementById("gototop");
+    button2.classList.toggle("dark-button");
 
     var x = document.getElementById("lightmodecaption");
     if (x.innerHTML === "Toggle Light Mode") {
